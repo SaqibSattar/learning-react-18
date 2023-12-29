3e798f2061f0ac8585137010b02dfa6355e08b67
SaqibSattar <saqibkhanz1234@gmail.com>
2023-12-29 15:31:50 +0400

    class-component implemented

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <>
+            <h1>Hello from Welcome class component</h1>
+            </>
+        )
+    }
+}
+
+export default Welcome;
