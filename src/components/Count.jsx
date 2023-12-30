31bc8f2ca30e9364265653d69f91b0ad8dcb3c17
SaqibSattar <saqibkhanz1234@gmail.com>
2023-12-30 13:11:29 +0400

    implement decrement feature in count component

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,52 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Count extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            count: 0
-        }
-    }
+    this.state = {
+      count: 0,
+    };
+  }
 
-    incrementCount() {
-        // this.setState({
-        //     count: this.state.count + 1
-        // }, () => {
-        //     console.log(this.state.count)
-        // })
+  incrementCount() {
+    // this.setState({
+    //     count: this.state.count + 1
+    // }, () => {
+    //     console.log(this.state.count)
+    // })
 
-        // this.setState((prevState) => ({
-        //    count: prevState + 1
-        // }))
+    // this.setState((prevState) => ({
+    //    count: prevState + 1
+    // }))
 
-        this.setState((prevState) => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
+
+  decrementCount() {
+    this.setState((prevState) => {
             return {
-                count: prevState.count + 1
-            };
-        });
-        
-    }
+                count: prevState.count < 1 ? 0 : prevState.count - 1,
+            }
+    });
+  }
 
-    incrementCountByFive() {
-        this.incrementCount()
-        this.incrementCount()
-        this.incrementCount()
-        this.incrementCount()
-        this.incrementCount()
-    }
-    
-    render() {
-        return (
-            <div>
-            <h3>
-            Count - {this.state.count} 
-            </h3>
-            <button onClick={() => {this.incrementCount()}}>increment by One</button>
-            <button onClick={() => {this.incrementCountByFive()}}>increment by Five</button>
-            </div>
-        )
-    }
+  incrementCountByFive() {
+    this.incrementCount();
+    this.incrementCount();
+    this.incrementCount();
+    this.incrementCount();
+    this.incrementCount();
+  }
+
+  decrementCountByFive() {
+    this.decrementCount();
+    this.decrementCount();
+    this.decrementCount();
+    this.decrementCount();
+    this.decrementCount();
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Count - {this.state.count}</h3>
+        <button
+          onClick={() => {
+            this.incrementCount();
+          }}
+        >
+          increment by One
+        </button>
+        <button
+          onClick={() => {
+            this.incrementCountByFive();
+          }}
+        >
+          increment by Five
+        </button>
+        <button
+          onClick={() => {
+            this.decrementCount();
+          }}
+        >
+          decrement by One
+        </button>
+        <button
+          onClick={() => {
+            this.decrementCountByFive();
+          }}
+        >
+          decrement by Five
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Count;
