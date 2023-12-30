38e981c54afeb47290c7ba1f683c1105df696457
SaqibSattar <saqibkhanz1234@gmail.com>
2023-12-30 11:04:58 +0400

    render component using jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <>
-      <Greet />
-      <Welcome />
+     {/* <Greet />
+      <Welcome />*/}
+      <Hello />
     </>
   );
 }
+console.log(<Greet />)
 
 export default App;
