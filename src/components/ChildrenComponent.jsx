1a63d33f7a0f3e8dbfbbe387518f4c20ff944474
SaqibSattar <Saqibkhanz1234@gmail.com>
2024-01-02 00:19:19 +0500

    passing data from child-to-parent

--- src/components/ChildrenComponent.jsx
+++ src/components/ChildrenComponent.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const ChildrenComponent = (props) => {
   return (
     <div>
-      <button onClick={props.greetHandle}>Click</button>{' '}
+      <button
+        onClick={() => {
+          props.greetHandle('Child')
+        }}
+      >
+        Click
+      </button>{' '}
     </div>
   )
 }
