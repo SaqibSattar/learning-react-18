7ab60cba7c504bb7cba8f3f47320a5dceb36a270
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-03 13:09:58 +0400

    refactor state variables inside render method

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,59 +1,60 @@
 import React, { Component } from "react";
 
 class Form extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         username: '',
-         comment: '',
-         topic: 'React'
-      }
-    }
-    handleUsernameChange = (e) => {
-        this.setState({
-            username: e.target.value
-        })
-    }
+  constructor(props) {
+    super(props);
 
-    handleCommentChange = (e) => {
-        this.setState({
-            comment: e.target.value
-        })
-    }
+    this.state = {
+      username: "",
+      comment: "",
+      topic: "React",
+    };
+  }
+  handleUsernameChange = (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  };
 
-    handleTopicChange = (e) => {
-        this.setState({
-            topic: e.target.value
-        })
-    }
+  handleCommentChange = (e) => {
+    this.setState({
+      comment: e.target.value,
+    });
+  };
 
-    handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(this.state)
+  handleTopicChange = (e) => {
+    this.setState({
+      topic: e.target.value,
+    });
+  };
 
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
   render() {
+    const { username, comment, topic } = this.state;
+
     return (
       <form onSubmit={this.handleSubmit}>
         <div>
-        <label>Username</label>
-        <input value={this.state.username} onChange={this.handleUsernameChange}/>
+          <label>Username</label>
+          <input value={username} onChange={this.handleUsernameChange} />
         </div>
         <div>
-        <br />
-        <label>Comment</label>
-        <textarea value={this.state.comment} onChange={this.handleCommentChange}/>
+          <br />
+          <label>Comment</label>
+          <textarea value={comment} onChange={this.handleCommentChange} />
         </div>
         <div>
-        <br />
-        <label>Topic</label>
-        <select value={this.state.topic} onChange={this.handleTopicChange} >
-        <option value='React'>React</option>
-        <option value='Angular'>Angular</option>
-        <option value='Vue'>Vue</option>
-        </select>
+          <br />
+          <label>Topic</label>
+          <select value={topic} onChange={this.handleTopicChange}>
+            <option value="React">React</option>
+            <option value="Angular">Angular</option>
+            <option value="Vue">Vue</option>
+          </select>
         </div>
         <button type="submit">Submit</button>
       </form>
