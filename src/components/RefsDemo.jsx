fafdd66143096e984b8f41479fde17f86881c2af
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-08 10:46:15 +0400

    implement callback refs

--- src/components/RefsDemo.jsx
+++ src/components/RefsDemo.jsx
@@ -4,11 +4,18 @@ export class RefsDemo extends Component {
     constructor(props) {
       super(props)
     this.inputRef = React.createRef();
+    this.cbRef = null
+    this.setCbRef = element => {
+        this.cbRef = element
+    }
     }
 
     componentDidMount() {
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
+        if (this.cbRef) {
+            this.cbRef.focus()
+        }
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
     }
 
     handleClick = () => {
@@ -19,6 +26,8 @@ export class RefsDemo extends Component {
     return (
       <div>
       <input type='text' ref={this.inputRef}/>
+      <br />
+      <input type='text' ref={this.setCbRef}/>
       <br />
       <button onClick={this.handleClick}>Submit</button>
       </div>
