0d75965bb137ae8eda3bcf5b43b28737e76e2977
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-08 11:12:42 +0400

    implement Refs with Class components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import './App.css'
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Count from './components/Count'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBinding from './components/EventBinding'
-import ParentComponent from './components/ParentComponent'
-import UserGreeting from './components/UserGreeting'
-import NameList from './components/NameList'
-import TodoList from './components/TodoList'
-import Stylesheet from './components/Stylesheet'
-import Inline from './components/Inline'
-import Form from './components/Form'
-import styles from './appStyles.module.css'
-import './appStyles.css'
-import LifecycleA from './components/LifecycleA'
-import FragmentDemo from './components/FragmentDemo'
-import Table from './components/Table'
-import ParentComp from './components/ParentComp'
-import RefsDemo from './components/RefsDemo'
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
+import Count from "./components/Count";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBinding from "./components/EventBinding";
+import ParentComponent from "./components/ParentComponent";
+import UserGreeting from "./components/UserGreeting";
+import NameList from "./components/NameList";
+import TodoList from "./components/TodoList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import Form from "./components/Form";
+import styles from "./appStyles.module.css";
+import "./appStyles.css";
+import LifecycleA from "./components/LifecycleA";
+import FragmentDemo from "./components/FragmentDemo";
+import Table from "./components/Table";
+import ParentComp from "./components/ParentComp";
+import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
 
 function App() {
   return (
     <>
-    <RefsDemo />
-      {/*  <ParentComp />
+      <FocusInput />
+      {/* <RefsDemo />
+       <ParentComp />
       <Table />
      <FragmentDemo />
       <LifecycleA />
@@ -52,7 +54,7 @@ function App() {
       <Greet name="user3" />
         <Hello /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
