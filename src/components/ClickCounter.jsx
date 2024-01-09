9bc45d85149ef437ac03252804142316bac15a1a
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-09 12:29:06 +0400

    implement  Higher Order Components (Part 2)

--- src/components/ClickCounter.jsx
+++ src/components/ClickCounter.jsx
@@ -1,29 +1,14 @@
 import React, { Component } from "react";
-
+import withCounter from "./withCounter";
 export class ClickCounter extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: 0,
-    };
-  }
-
-  incrementCount = () => {
-    this.setState((prevState) => {
-      console.log(prevState);
-      return { count: prevState.count + 1 };
-    });
-  };
-
   render() {
-    const { count } = this.state;
+    const { count, incrementCount } = this.props;
     return (
       <div>
-        <button onClick={this.incrementCount}>Clicked {count} times</button>
+        <button onClick={incrementCount}>Clicked {count} times</button>
       </div>
     );
   }
 }
 
-export default ClickCounter;
+export default withCounter(ClickCounter);
--- src/components/HoverCounter.jsx
+++ src/components/HoverCounter.jsx
@@ -1,29 +1,14 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import withCounter from "./withCounter";
 export class HoverCounter extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          count: 0,
-        };
-      }
-    
-      incrementCount = () => {
-        this.setState((prevState) => {
-          console.log(prevState);
-          return { count: prevState.count + 1 };
-        });
-      };
-    
-      render() {
-        const { count } = this.state;
-        return (
-          <div>
-            <h4 onMouseOver={this.incrementCount}>Hovered {count} times</h4>
-          </div>
-        );
-      }
-    }
+  render() {
+    const { count, incrementCount } = this.props;
+    return (
+      <div>
+        <h4 onMouseOver={incrementCount}>Hovered {count} times</h4>
+      </div>
+    );
+  }
+}
 
-export default HoverCounter
+export default withCounter(HoverCounter);
